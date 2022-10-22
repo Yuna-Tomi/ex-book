a5605cab451794641e3e90c037c86e1534c20fe1
Yuna Tomida <ytomida.mmm@gmail.com>
2022-10-22 14:24:00 +0900

    follow chapter 17

---
+++ programming-elixir/ch17/sequence/lib/sequence.ex
@@ -0,0 +1,25 @@
+defmodule Sequence do
+  @server Sequence.Server
+  @moduledoc """
+  カウンタを提供するサーバ。
+  """
+
+  @doc """
+  初期値 initial_number からカウント開始
+
+  ## Examples
+      iex> Sequence.start_link(10)
+      :ok, #PID<...>
+  """
+  def start_link(initial_number), do: GenServer.start_link(@server, initial_number, @server)
+
+  @doc """
+  1 カウントする。戻り値は現在の値。
+  """
+  def next_number, do: GenServer.call(@server, :next_number)
+
+  @doc """
+  delta だけカウントする。戻り値なし。(:ok)
+  """
+  def increment_number(delta), do: GenServer.cast(@server, {:increment_number, delta})
+end
