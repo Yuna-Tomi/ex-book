8d572bed14457c7bc7484919c85fd3cdeebf79b9
Yuna Tomida <ytomida.mmm@gmail.com>
2022-10-22 19:26:44 +0900

    follow chapter 20

---
+++ programming-elixir/ch20/sequence/mix.exs
@@ -0,0 +1,34 @@
+defmodule Sequence.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sequence,
+      version: "0.2.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Sequence.Application, 100},
+      registered: [
+        # ライブラリも含め名前が一意であることを保証
+        Sequence.Server
+      ]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:distillery, "~> 2.1", runtime: false}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
